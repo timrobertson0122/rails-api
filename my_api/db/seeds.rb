5da15f6d6099161ad1267310f898eeba4dd93f6d
Tim <timrobertson0122@gmail.com>
2016-11-14 14:43:01 +0000

    built rails api

---
+++ my_api/db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+Product.create(name: 'Smart Hub', price: 49.99)
+Product.create(name: 'Motion Sensor', price: 24.99)
+Product.create(name: 'Wireless Camera', price: 99.99)
+Product.create(name: 'Smoke Sensor', price: 19.99)
+Product.create(name: 'Water Leak Sensor', price: 14.99)
